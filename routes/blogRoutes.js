87195ba0df30e7f6287f1a168bbda86b997c9b75
Rcan3624 <55251953+Rcan3624@users.noreply.github.com>
2023-03-04 19:37:18 -0500

    Created blogRoutes and made changes to app file

---
+++ routes/blogRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const router = express.Router();
+
+// blog routes
+
+router.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  // https://youtu.be/VVGgacjzc2Y?list=PLKycv9hdyoo3VewM8LEcQ8Iis-qgjwTtG&t=389
+  router.post('/blogs', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+  
+    blog.save()
+      .then(result => {
+        res.redirect('/blogs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  router.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+      .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  router.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  module.exports
